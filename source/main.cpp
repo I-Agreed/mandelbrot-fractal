8f98b2769e80a9214be53d80cf082afb3feefd2d
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2021-03-25 20:11:21 +1100

    fixed problem with scaled y being an integer
    still looks a bit stretched

--- source/main.cpp
+++ source/main.cpp
@@ -2,29 +2,25 @@
 
 const int WIDTH = 640;
 const int HEIGHT = 480;
-const int loops = 100;
+const int loops = 500;
 SDL_Window* window = SDL_CreateWindow("Mandelbrot", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
                                          WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 void handle_point(int x, int y) {
     float scaledX = x*3.5/WIDTH-2.5;
-    float scaledY = y*2/HEIGHT-1;
+    float scaledY = y*2.0/HEIGHT-1;
     float x2 = 0;
     float y2 = 0;
     float x3 = 0;
-    float i;
+    int i = 0;
     while (i < loops && x2*x2 + y2*y2 <= 4) {
         x3 = x2*x2 - y2*y2 + scaledX;
         y2 = 2*x2*y2 + scaledY;
         x2 = x3;
         i++;
     }
-    if (i > 50) {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    } else {
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, SDL_ALPHA_OPAQUE);
-    }
+    SDL_SetRenderDrawColor(renderer, i/500.0*255, i/500.0*255, i/500.0*255, SDL_ALPHA_OPAQUE);
     SDL_RenderDrawPoint(renderer, x, y);
 }
 
